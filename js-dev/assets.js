5db2ebb4d3d2c71845c34f11aab53a648da094b8
Andrew <andreiduffy@gmail.com>
2016-09-26 14:33:29 +0300

    add customsrollbar & device.js

--- js-dev/assets.js
+++ js-dev/assets.js
@@ -3,3 +3,5 @@
 //@prepros-append vendors/maskedinput.js
 //@prepros-append vendors/fancy/fancybox.js
 //@prepros-append vendors/velocity.js
+//@prepros-append vendors/custom_scroll.js
+//@prepros-append vendors/device.min.js
