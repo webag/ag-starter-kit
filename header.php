7b9029f940fe322c0172dab49aa6292d4776665e
Andrew <andreiduffy@gmail.com>
2016-09-29 15:13:50 +0300

    added og meta tags

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="format-detection" content="telephone=no">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!--[if lt IE 9]>
@@ -10,10 +11,17 @@
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 
-	<meta name="format-detection" content="telephone=no">
-	<title>Шаблон проекта</title>
-	<link href="css/main.css" rel="stylesheet">
 
+	<title>Шаблон проекта</title>
+
+	<!-- og -->
+	<meta property="og:title" content="Шаблон проекта">
+	<meta property="og:description" content="Описание проекта">
+	<meta property="og:type" content="website">
+	<meta property="og:image" content="/img/og.jpg">
+	<!-- og -->
+
+	<link href="css/main.css" rel="stylesheet">
 	<script src="js/assets.js" type="text/javascript" ></script>
 	<script src="js/main.js" type="text/javascript" ></script>
 </head>
