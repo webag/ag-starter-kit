0ce4e32c9e147442e5b237baefb3476caf570b57
Andrew <andreiduffy@gmail.com>
2017-12-06 14:55:56 +0300

    new fancybox,fixes,remove scroll

--- index.php
+++ index.php
@@ -3,14 +3,25 @@
 <section>
 	<div class="container">
 		<h1>Hello from the other side</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur distinctio minima placeat, possimus quasi suscipit! A asperiores dicta fuga necessitatibus nobis numquam obcaecati perspiciatis. Aliquid eaque minima minus modi voluptates.</p>
 		<h2>Hello from the other side</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur distinctio minima placeat, possimus quasi suscipit! A asperiores dicta fuga necessitatibus nobis numquam obcaecati perspiciatis. Aliquid eaque minima minus modi voluptates.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur distinctio minima placeat, possimus quasi suscipit! A asperiores dicta fuga necessitatibus nobis numquam obcaecati perspiciatis. Aliquid eaque minima minus modi voluptates.</p>
 		<h3>Hello from the other side</h3>
-		<p>Экстренная помощь MIG</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur distinctio minima placeat, possimus quasi suscipit! A asperiores dicta fuga necessitatibus nobis numquam obcaecati perspiciatis. Aliquid eaque minima minus modi voluptates.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur distinctio minima placeat, possimus quasi suscipit! A asperiores dicta fuga necessitatibus nobis numquam obcaecati perspiciatis. Aliquid eaque minima minus modi voluptates.</p>
+		<h4>Hello from the other side</h4>
 		<ul>
 			<li>Экстренная помощь MIG</li>
 			<li>Экстренная помощь MIG</li>
 			<li>Экстренная помощь MIG</li>
 		</ul>
+		<h5>Hello from the other side</h5>
+		<ol>
+			<li>Экстренная помощь MIG</li>
+			<li>Экстренная помощь MIG</li>
+			<li>Экстренная помощь MIG</li>
+		</ol>
 
 		<a href="#" class="fancy btn" data-src="#modal-order">Открыть order</a>
 		<a href="img/img.png" class="fancy btn" data-fancybox="1">Открыть img</a>
@@ -18,17 +29,6 @@
 		<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="fancy-video btn">Открыть youtube</a>
 		<a href="https://yandex.ru/map-widget/v1/-/CBQneLhsOA" class="fancy-map btn" data-type="iframe">схема проезда</a>
 
-		<div class="scroll">
-			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент.</p>
-			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
-			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
-			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
-			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
-			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
-			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
-			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
-			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
-		</div>
 
 		<div class="row">
 			<div class="grid-3">3333</div>
