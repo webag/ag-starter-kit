d3166ddc62dc42af5d489d0c6e2bc346c6fd8733
Andrew <andreiduffy@gmail.com>
2017-12-15 10:28:03 +0300

    some fixes

--- js/main.js
+++ js/main.js
@@ -89,10 +89,7 @@ function init_fancy() {
 		backFocus: false,
 		animationEffect: "zoom-in-out",
 		animationDuration: 300,
-		transitionEffect: "slide",
-		btnTpl: {
-			smallBtn: '<button data-fancybox-close class="fancybox-close-corner" title="Закрыть"></button>'
-		}
+		transitionEffect: "slide"
 	});
 	$('.fancy-map').fancybox({
 		toolbar: false,
@@ -100,10 +97,7 @@ function init_fancy() {
 		backFocus: false,
 		animationEffect: "zoom-in-out",
 		animationDuration: 300,
-		transitionEffect: "slide",
-		btnTpl: {
-			smallBtn: '<button data-fancybox-close class="fancybox-close-corner" title="Закрыть"></button>'
-		}
+		transitionEffect: "slide"
 	});
 }
 
@@ -115,9 +109,6 @@ function init_fancy__video() {
 		animationEffect: "zoom-in-out",
 		animationDuration: 300,
 		transitionEffect: "slide",
-		btnTpl: {
-			smallBtn: '<button data-fancybox-close class="fancybox-close-corner" title="Закрыть"></button>'
-		},
 		youtube: {
 			controls: 1,
 			showinfo: 0,
